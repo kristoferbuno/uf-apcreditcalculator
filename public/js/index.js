101d10731611f8e0413f56d51a46eaf99b1ed587
Kristofer Buno <krisbuno@gmail.com>
2019-03-19 02:35:24 -0400

    Calculate and display credits on submit

--- public/js/index.js
+++ public/js/index.js
@@ -1,62 +1,77 @@
-var apexamcredits, apexams;
+var ap_exam_credits, ap_exams;
 
-//pulling in from the apexamcredits.json file stored in /public/json
+//pulling in from the ap_exam_credits.json file stored in /public/json
 $.getJSON(`json\\apexamcredits.json`, function(creds) {
-    console.log(`success`)
-
     //turning JSON file into a JS object, then putting in the exam names into an array
-    apexamcredits = JSON.parse(JSON.stringify(creds));
-    apexams = Object.keys(apexamcredits[3])
+    ap_exam_credits = JSON.parse(JSON.stringify(creds));
+    ap_exams = Object.keys(ap_exam_credits[3])
+
+    //programatically add DOM elements with each exam and a field for each exam after JSON is loaded
+    for (var exam in ap_exams)
+    {
+    var optel = `
+                <div class="ui accordion fluid field">
+                    <div class="title">
+                        <i class="icon dropdown"></i>
+                        ${ap_exams[exam]}
+                    </div>
+                <div class="fluid content field">
+                    <input placeholder="Score (between 3 and 5)" name="${ap_exams[exam]}" min="3" max="5" type="number">
+                </div>
+                `;
+        
+        $("#listtop").append(optel);
+    } 
+    $('.ui.accordion').accordion();
 })
 
 //things to do when document becomes ready
 $(document).ready(function(){
     //instantiate fomantic ui dropdown js
     $('#hi').dropdown();
-    
-    //append each exam in apexamcredits
-    for (var exam in apexams)
-    {
-        
-    var optel = `
-                <div class="ui accordion field">
-                    <div class="title">
-                        <i class="icon dropdown"></i>
-                        ${apexams[exam]}
-                    </div>
-                <div class="content field">
-                    <input placeholder="Score" type="number">
-                </div>
-                `;
-        
-        $("#listtop").append(optel);
-
-//    $(apexams[exam]).innerHTML = apexams[exam];
-    }   
-    $('.ui.accordion').accordion();
 })
 
-//onclick function when you click one of the exam items
-function examClick(e) {
-    //toggles style and id
-    e.style = (e.id == `selected`) ? `background-color:transparent; color: rgba(0, 0, 0, 0.4)` : `background-color:#1080b0; color:#FFFFFF`;
-    e.id = e.id == `selected` ? `` : `selected`;
+//transitions from selection view to scores view
+function submitForm()
+{
+    //show credits and remove the exam selection screen
+    $('#selection_container').transition('fade');
+    $('#credits_container').transition('fade', '1000ms');
 
+    let formData = $('form').serializeArray();
+    let scores = {};
 
-    if (e.id == `selected`)
-    {
-        if (e.children.length == 2)
-        {
-        var scoreBox = document.createElement("input");
-        scoreBox.className = `content field active`;
-        scoreBox.type = `number`;
-        scoreBox.id = `${e.innerHTML} score`;
-        scoreBox.display = `inline`;
-        e.append(scoreBox);
-        }
-        else
-        {
-        e.children[1].class = e.children[1].class == `field` ? `disabled field` : `field`;
+    for (var index in formData){
+        if (formData[index].value != ''){
+            scores[formData[index].name] = formData[index].value;
         }
     }
+
+    let credit_earned = {
+        "c":0,
+        "m":0,
+        "h":0,
+        "bp":0,
+        "s":0,
+        "n":0,
+        "d":0,
+        "total":0
+    }
+
+    for (var exam in scores){
+        for (var category in credit_earned)
+        {
+            let score = scores[exam];
+            credit_earned[category] += ap_exam_credits[score][exam][category];
+        }
+    }
+
+    $('#composition_earned').text(credit_earned.c);
+    $('#mathematics_earned').text(credit_earned.m);
+    $('#humanities_earned').text(credit_earned.m);
+    $('#sciences_earned').text(credit_earned.bp);
+    $('#socsci_earned').text(credit_earned.s);
+    $('#international_earned').text(credit_earned.n);
+    $('#diveristy_earned').text(credit_earned.d);
+    $('#total_earned').text(credit_earned.total);
 }
